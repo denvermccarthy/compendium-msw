96c884096107b402aa763b361507994e619def91
Denver McCarthy <dmichaelspdx@gmail.com>
2022-05-02 17:42:48 -0700

    added tests for second suite

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -4,7 +4,7 @@ export default function Quote({ character, quote, image }) {
   return (
     <div>
       <h4>{character}</h4>
-      <img src={image} alt="" />
+      <img src={image} alt={`A picture of ${character}`} />
       <p>{quote}</p>
     </div>
   );
--- src/views/QuoteView/QuoteView.test.jsx
+++ src/views/QuoteView/QuoteView.test.jsx
@@ -1,5 +1,16 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, getByRole } from '@testing-library/react';
+import { mockData } from '../../tests/fixtures/mockData';
+import QuoteView from './QuoteView';
 
 describe('View component renders cards correctly', () => {
-  it.skip('cards should have a title, image and a quote', () => {});
+  it('cards should have a title, image and a quote', () => {
+    render(<QuoteView quotes={mockData} />);
+
+    screen.getByRole('list');
+    screen.getByText('Homer Simpson');
+    screen.getByAltText('A picture of Homer Simpson');
+    screen.getByText(
+      `Facts are meaningless. You could use facts to prove anything that's even remotely true.`
+    );
+  });
 });
